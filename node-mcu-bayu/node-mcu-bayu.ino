0345e9115aa448337a3e1b6cef76f40456c3a9ab
Nurrochmat Saptoaji <nurrochmat.sapto@gmail.com>
2020-05-10 06:29:45 +0700

    commit terakhir
    disini cuman diubah bagian bayu.ino

--- node-mcu-bayu/node-mcu-bayu.ino
+++ node-mcu-bayu/node-mcu-bayu.ino
@@ -1,9 +1,15 @@
+
+#include <ESP8266WiFi.h>
 #include <Wire.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+const char *ssid = "SWIFT-3-OBOHIB0 8693"; //nama wifi
+const char *password = "rawamas4498";      //password
+const char *host = "10.10.12.152";         //IP PC
+
 //memasukkan pin
-int sensorPinDS18b20 = 2;
+int sensorPinDS18b20 = 4;
 
 //membuat variabel kosong
 float suhuC;
@@ -14,6 +20,24 @@ DallasTemperature sensors(&ourWire);
 void setup() {
    Serial.begin(115200);
    delay(1000);
+
+   Serial.println();
+   Serial.println();
+   Serial.print("Connecting to ");
+   Serial.println(ssid);
+
+   WiFi.begin(ssid, password);
+
+   while (WiFi.status() != WL_CONNECTED)
+   {
+     delay(500);
+     Serial.print(".");
+   }
+
+   Serial.println("");
+   Serial.println("WiFi connected");
+   Serial.println("IP address: ");
+   Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -25,4 +49,5 @@ void loop() {
   Serial.print(suhuC);
   Serial.print(" ");
   Serial.println("C");
+  delay(1000);
 }
